8a6ff16a40dc6fedee159094a21734a84f24a8fb
Apeirogon99 <apeirogon99@gmail.com>
2025-11-12 10:16:57 +0900

    register app config
    - user repo service 연결
    - user product repo service 연결
    - admin repo service 연결

--- src/main/java/com/mini/timedeal/config/AppConfig.java
+++ src/main/java/com/mini/timedeal/config/AppConfig.java
@@ -1,8 +1,17 @@
 package com.mini.timedeal.config;
 
+import com.mini.timedeal.domain.admin.mapper.AdminMapper;
+import com.mini.timedeal.domain.admin.service.AdminService;
+import com.mini.timedeal.domain.admin.storage.AdminRepository;
 import com.mini.timedeal.domain.promotion.mapper.PromotionMapper;
 import com.mini.timedeal.domain.promotion.service.PromotionService;
 import com.mini.timedeal.domain.promotion.storage.PromotionRepository;
+import com.mini.timedeal.domain.user.mapper.UserMapper;
+import com.mini.timedeal.domain.user.mapper.UserProductMapper;
+import com.mini.timedeal.domain.user.service.UserProductService;
+import com.mini.timedeal.domain.user.service.UserService;
+import com.mini.timedeal.domain.user.storage.UserProductRepository;
+import com.mini.timedeal.domain.user.storage.UserRepository;
 import org.apache.ibatis.session.SqlSession;
 
 public final class AppConfig {
@@ -16,9 +25,15 @@ public final class AppConfig {
 
         // Repository 등록
         context.registerBean(PromotionRepository.class);
+        context.registerBean(AdminRepository.class);
+        context.registerBean(UserRepository.class);
+        context.registerBean(UserProductRepository.class);
 
         // Service 등록
         context.registerBean(PromotionService.class, PromotionMapper.class);
+        context.registerBean(AdminService.class, AdminMapper.class);
+        context.registerBean(UserService.class, UserMapper.class);
+        context.registerBean(UserProductService.class, UserProductMapper.class);
     }
 
     public void initMyBatis() {
@@ -26,10 +41,15 @@ public final class AppConfig {
         SqlSession sqlSession = MyBatisConfig.getSqlSessionFactory().openSession(true);
         
         // Mapper 등록
-        PromotionMapper promotionMapper = sqlSession.getMapper(PromotionMapper.class);
-        context.registerBeanObject(promotionMapper);
+        context.registerBeanObject(sqlSession.getMapper(PromotionMapper.class));
+        context.registerBeanObject(sqlSession.getMapper(AdminMapper.class));
+        context.registerBeanObject(sqlSession.getMapper(UserMapper.class));
+        context.registerBeanObject(sqlSession.getMapper(UserProductMapper.class));
         
         // Service 등록
         context.registerBean(PromotionService.class, PromotionMapper.class);
+        context.registerBean(AdminService.class, AdminMapper.class);
+        context.registerBean(UserService.class, UserMapper.class);
+        context.registerBean(UserProductService.class, UserProductMapper.class);
     }
 }
--- src/test/java/com/mini/timedeal/domain/AppContextTests.java
+++ src/test/java/com/mini/timedeal/domain/AppContextTests.java
@@ -2,9 +2,12 @@ package com.mini.timedeal.domain;
 
 import com.mini.timedeal.config.AppConfig;
 import com.mini.timedeal.config.AppContext;
+import com.mini.timedeal.domain.admin.service.AdminService;
 import com.mini.timedeal.domain.promotion.mapper.PromotionMapper;
 import com.mini.timedeal.domain.promotion.service.PromotionService;
 import com.mini.timedeal.domain.promotion.storage.PromotionRepository;
+import com.mini.timedeal.domain.user.service.UserProductService;
+import com.mini.timedeal.domain.user.service.UserService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
@@ -19,14 +22,48 @@ public class AppContextTests {
     }
 
     @Test
-    @DisplayName("컨텍스트 테스트")
-    public void promotionTest() {
+    @DisplayName("컨텍스트 저장소 초기화 테스트")
+    public void initRepositoryTest() {
         AppContext context = AppContext.getInstance();
         config.initRepository();
 
-        PromotionService service1 = context.getBean(PromotionService.class);
-        PromotionService service2 = context.getBean(PromotionService.class);
+        AdminService adminService1 = context.getBean(AdminService.class);
+        AdminService adminService2 = context.getBean(AdminService.class);
+        Assertions.assertEquals(adminService1, adminService2);
 
-        Assertions.assertEquals(service1, service2);
+        PromotionService promotionService1 = context.getBean(PromotionService.class);
+        PromotionService promotionService2 = context.getBean(PromotionService.class);
+        Assertions.assertEquals(promotionService1, promotionService2);
+
+        UserService userService1 = context.getBean(UserService.class);
+        UserService userService2 = context.getBean(UserService.class);
+        Assertions.assertEquals(userService1, userService2);
+
+        UserProductService userProductService1 = context.getBean(UserProductService.class);
+        UserProductService userProductService2 = context.getBean(UserProductService.class);
+        Assertions.assertEquals(userProductService1, userProductService2);
+    }
+
+    @Test
+    @DisplayName("컨텍스트 MyBatis 초기화 테스트")
+    public void initMyBatisTest() {
+        AppContext context = AppContext.getInstance();
+        config.initMyBatis();
+
+        AdminService adminService1 = context.getBean(AdminService.class);
+        AdminService adminService2 = context.getBean(AdminService.class);
+        Assertions.assertEquals(adminService1, adminService2);
+
+        PromotionService promotionService1 = context.getBean(PromotionService.class);
+        PromotionService promotionService2 = context.getBean(PromotionService.class);
+        Assertions.assertEquals(promotionService1, promotionService2);
+
+        UserService userService1 = context.getBean(UserService.class);
+        UserService userService2 = context.getBean(UserService.class);
+        Assertions.assertEquals(userService1, userService2);
+
+        UserProductService userProductService1 = context.getBean(UserProductService.class);
+        UserProductService userProductService2 = context.getBean(UserProductService.class);
+        Assertions.assertEquals(userProductService1, userProductService2);
     }
 }
